b866f453d258d3d139d79f780c5dd9f88eecb4fa
Matthew <matthew_mo@hotmail.co.uk>
2017-08-09 20:06:17 +0100

    First draft of IntVector class

--- DataStructures/src/datastructures/IntVector.java
+++ DataStructures/src/datastructures/IntVector.java
@@ -5,6 +5,8 @@
  */
 package datastructures;
 
+import java.util.Vector;
+
 /**
  *
  * @author matth
@@ -16,34 +18,58 @@ public class IntVector {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        IntVector intVector = new IntVector();
+        int a = 1, b = 2, c = 3;
+        intVector.insertEnd(a);
+        intVector.insertEnd(b);
+        intVector.insertEnd(c);
+        intVector.getIndex(1);
+        intVector.setIndex(2, 11);
+        System.out.println(intVector.size());
+        for(int i = 0; i < intVector.size(); i++)
+        {
+            System.out.printf("%d ", intVector.getIndex(i));
+        }
     }
 
+    private Vector vector;
+    
     public IntVector()
     {
-        
+        vector = new Vector();
     }
     
     // Add a new entry to end of array
     public void insertEnd(int a)
     {
-        
+        vector.add(a);
     }
     
     // Get value at an index
     public int getIndex(int index)
     {
-        return 0;
+        return (int) vector.get(index);
     }
     
     // Set value at an index
-    public int setIndex(int index, int value)
+    public void setIndex(int index, int value)
     {
-        return 0;
+        vector.set(index, value);
     }
     
     // return size
     public int size()
     {
-        return 0;
+        return vector.size();
+    }
+    
+    public Vector getVector()
+    {
+        return vector;
+    }
+    
+    public void setVector(Vector vector)
+    {
+        this.vector = vector;
     }
 }

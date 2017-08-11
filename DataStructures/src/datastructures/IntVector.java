dcea91f32bef6c3d2e726689017f6b86ef63a9f6
Matthew <matthew_mo@hotmail.co.uk>
2017-08-11 12:51:49 +0100

    Completed first draft of intVector that (I think) satisfies the spec

--- DataStructures/src/datastructures/IntVector.java
+++ DataStructures/src/datastructures/IntVector.java
@@ -19,20 +19,36 @@ public class IntVector {
     public static void main(String[] args) {
         // TODO code application logic here
         int valueInsert = 11;
-        int index = 2;
+        int getIndex = 2;
+        int setIndex = 0;
+        int value = 100;
         
         IntVector intVector = new IntVector();
         intVector.insertEnd(valueInsert);
+        System.out.println("This is the vanilla array:");
+        for(int i = 0; i < intVector.array.length; i++)
+        {
+            System.out.print(intVector.array[i] + " ");
+        }
+        System.out.println("");
         System.out.println("This is the following array when insertEnd method is called with the value " + valueInsert + " inserted");
+        intVector.printNewArray(intVector);
+        System.out.println("");
+        System.out.println("This is the following size of the array");
+        System.out.println(intVector.size());
+        System.out.println("This is the value at at a particular index " + getIndex);
+        System.out.println(intVector.getIndex(getIndex));
+        System.out.println("This is the array when set index " + setIndex + " with value " + value);
+        intVector.setIndex(setIndex, value);
+        intVector.printNewArray(intVector);
+    }
+    
+    public void printNewArray(IntVector intVector)
+    {
         for(int i = 0; i < intVector.newArray.length; i++)
         {
             System.out.print(intVector.newArray[i] + " ");
         }
-        System.out.println("");
-        System.out.println("This is the following size of the array");
-        System.out.println(intVector.size());
-        System.out.println("This is the value at at a particular index " + index);
-        System.out.println(intVector.getIndex(index));
     }
 
     private int[] array;
@@ -75,7 +91,19 @@ public class IntVector {
     // Set value at an index
     public void setIndex(int index, int value)
     {
-        
+        newArray = new int[array.length * 2];
+        int count = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            if(i == index)
+            {
+                newArray[i] = value;
+            }
+            else
+            {
+                newArray[i] = array[i];
+            }
+        }
     }
     
     // return size
@@ -83,7 +111,7 @@ public class IntVector {
     {
         int count = 0;
         
-        for(int i : newArray)
+        for(int i : array)
         {
             count++;
         }
